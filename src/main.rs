556d783756a344ba5d277fc9ba5cee01566f71ff
Matthew Downs <mwdowns@gmail.com>
2018-03-11 16:27:56 -0400

    fixes some code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
-extern crate futures;
+// extern crate futures;
 
 #[macro_use]
 extern crate dotenv_codegen;
 extern crate mysql_async as my;
-extern crate tokio_core as tokio;
+// extern crate tokio_core as tokio;
 
-use futures::Future;
-use my::prelude::*;
-use tokio::reactor::Core;
+// use futures::Future;
+// use my::prelude::*;
+// use tokio::reactor::Core;
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+// #[derive(Debug, PartialEq, Eq, Clone)]
 
 
 fn main() {
 
-    let mut builder = OptsBuilder::new();
-    builder.ip_or_hostname(dotenv!("DB_HOST"))
-        .db_name(dotevn!("DB_NAME"))
-        .user(dotenv!("DB_USER"))
-        .pass(dotenv!("DB_PASS"));
-    println!("{}", builder);
+    let mut builder = my::OptsBuilder::new();
+    let host = dotenv!("DB_HOST");
+    let db = dotenv!("DB_NAME");
+    let user = dotenv!("DB_USER");
+    let pass = dotenv!("DB_PASS");
+    builder.ip_or_hostname(host);
+    builder.db_name(Some(db));
+    builder.user(Some(user));
+    builder.pass(Some(pass));
+    println!("{:?}", builder);
 }
