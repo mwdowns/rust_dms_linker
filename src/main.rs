5a8545736924594bc37ada1c738634b70e180131
Matthew Downs <mwdowns@gmail.com>
2018-03-11 15:13:49 -0400

    some code to try and set up a connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+extern crate futures;
+
 #[macro_use]
 extern crate dotenv_codegen;
+extern crate mysql_async as my;
+extern crate tokio_core as tokio;
+
+use futures::Future;
+use my::prelude::*;
+use tokio::reactor::Core;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+
 
 fn main() {
-    let user = dotenv!("DB_USER");
-    println!("{}", user);
+
+    let mut builder = OptsBuilder::new();
+    builder.ip_or_hostname(dotenv!("DB_HOST"))
+        .db_name(dotevn!("DB_NAME"))
+        .user(dotenv!("DB_USER"))
+        .pass(dotenv!("DB_PASS"));
+    println!("{}", builder);
 }
